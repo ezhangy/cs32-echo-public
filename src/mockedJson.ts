e81a11e726dae7eadc16d62c40a49adc5bf7586c
jakobwismar <98495236+jakobwismar@users.noreply.github.com>
2023-02-16 19:11:24 -0500

    Updated mock csvs

--- src/mockedJson.ts
+++ src/mockedJson.ts
@@ -21,24 +21,25 @@ const mockLoadMap: { [fileName: string]: Array<Array<string | number>> } = {
 const stringCSVSearchMap: {
   [searchTerm: string]: Array<Array<string | number>>;
 } = {
-  ["tim"]: [stringCSV[0]],
-  ["nelson"]: [stringCSV[0]],
-  ["instructor"]: [stringCSV[0]],
-  ["john"]: [stringCSV[1]],
-  ["jane"]: [stringCSV[2]],
-  ["doe"]: [stringCSV[1], stringCSV[2]],
-  ["student"]: [stringCSV[1], stringCSV[2]],
+  ["[0, tim]"]: [stringCSV[0]],
+  ["[1, nelson]"]: [stringCSV[0]],
+  ["[2, instructor]"]: [stringCSV[0]],
+  ["[0, john]"]: [stringCSV[1]],
+  ["[0, jane]"]: [stringCSV[2]],
+  ["[1, doe]"]: [stringCSV[1], stringCSV[2]],
+  ["[2, student]"]: [stringCSV[1], stringCSV[2]],
 };
 
 const numberCSVSearchMap: {
   [searchTerm: string]: Array<Array<string | number>>;
 } = {
-  [0]: [numberCSV[0]],
-  [1]: [numberCSV[0]],
-  [2]: [numberCSV[0], numberCSV[1]],
-  [3]: [numberCSV[1], numberCSV[2]],
-  [4]: [numberCSV[1], numberCSV[2]],
-  [5]: [numberCSV[2]],
+  ["[0, 0]"]: [numberCSV[0]],
+  ["[1, 1]"]: [numberCSV[0]],
+  ["[0, 2]"]: [numberCSV[1]],
+  ["[2, 2]"]: [numberCSV[0]],
+  ["[1, 3]"]: [numberCSV[1], numberCSV[2]],
+  ["[2, 4]"]: [numberCSV[1], numberCSV[2]],
+  ["[0, 5]"]: [numberCSV[2]],
 };
 
 // Provide this to other modules (e.g., for testing!)
