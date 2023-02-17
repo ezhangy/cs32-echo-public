5e79ce5e836005d8eed5bae8ba09ebbb3e97658b
jakobwismar <98495236+jakobwismar@users.noreply.github.com>
2023-02-16 20:37:24 -0500

    Started DOM testing

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,6 +1,43 @@
 // all exports from main will now be available as main.X
 import * as main from "./main";
 
+// Lets us use DTL's query library
+import { screen } from "@testing-library/dom";
+// Lets us send user events (like typing and clicking)
+import userEvent from "@testing-library/user-event";
+
+// Template HTML for test running
+const startHTML = `
+<div class="repl-history" id="outputDiv">
+   <ul class="repl-output" title="Command Output"></ul>
+</div>
+<hr>
+<div class="repl-input">
+   <input type="text" class="repl-command-box" id="input-field" placeholder="Input text here.">
+   <button type="button" class="submit-button">Submit</button>
+</div>
+`;
+var submitButton: HTMLElement;
+var inputText: HTMLElement;
+beforeEach(function () {
+  main.clearHistory();
+  document.body.innerHTML = startHTML;
+
+  submitButton = screen.getByText("Submit");
+  inputText = screen.getByPlaceholderText("Input text here.");
+});
+
+//DOM tests
+
+test("testing empty input", function () {
+  userEvent.click(submitButton);
+  expect(
+    screen.getByTitle("Command Output").innerHTML == "submitted empty string"
+  );
+});
+
+//Other tests
+
 test("is 1 + 1 = 2?", () => {
   expect(1 + 1).toBe(2);
 });
--- src/main.ts
+++ src/main.ts
@@ -170,4 +170,5 @@ export {
   mockLoadMap,
   toggleVerbosity,
   setLoadedCSV,
+  clearHistory,
 };
